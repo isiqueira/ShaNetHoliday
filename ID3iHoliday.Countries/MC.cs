fd410332cb61c1eefd9a81778820602c93d5ba98
Shaenn <thomasgianini@gmail.com>
2018-02-21 08:31:13 +0100

    Work on country MC.

---
+++ ID3iHoliday.Countries/MC.cs
@@ -0,0 +1,114 @@
+﻿using ID3iCore;
+using ID3iDate;
+using ID3iHoliday.Models;
+using ID3iHoliday.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.DayOfWeek;
+using static ID3iHoliday.Syntax.Count;
+using static ID3iHoliday.Syntax.Month;
+using static ID3iHoliday.Models.RuleType;
+
+namespace ID3iHoliday.Countries
+{
+    /// <summary>
+    /// Définition pour Monaco.
+    /// </summary>
+    public class MC : Country
+    {
+        /// <summary>
+        /// Initialise une nouvelle instance pour la classe <see cref="MC"/>.
+        /// </summary>
+        public MC()
+        {
+            Code = "MC";
+            Alpha3Code = "MCO";
+            Names = NamesBuilder.Make.Add(Langue.EN, "Monaco").Add(Langue.FR, "Monaco").AsDictionary();
+            DaysOff.Add(Sunday);
+            Langues.Add(Langue.FR);
+            Rules = new ListRule()
+            {
+                new Rule()
+                {
+                    Expression = ExpressionTree.Date.Fix(On.January.The1st),
+                    Names = NamesBuilder.Make.Add(Langue.FR, "Nouvel An").AsDictionary()
+                },
+                new Rule()
+                {
+                    Expression = ExpressionTree.Observe.Fix(On.January.The27th).If(Sunday).Then.Next(Monday),
+                    Names = NamesBuilder.Make.Add(Langue.FR, "Sainte Dévote").AsDictionary(),
+                    Substitute = true
+                },
+                new Rule()
+                {
+                    Expression = ExpressionTree.Date.Catholic.GoodFriday,
+                    Names = NamesBuilder.Make.Add(Langue.FR, "Vendredi saint").AsDictionary(),
+                    Type = Observance
+                },
+                new Rule()
+                {
+                    Expression = ExpressionTree.Date.Catholic.Easter,
+                    Names = NamesBuilder.Make.Add(Langue.FR, "Pâques").AsDictionary(),
+                    Type = Observance
+                },
+                new Rule()
+                {
+                    Expression = ExpressionTree.Date.Catholic.EasterMonday,
+                    Names = NamesBuilder.Make.Add(Langue.FR, "Lundi de Pâques").AsDictionary()
+                },
+                new Rule()
+                {
+                    Expression = ExpressionTree.Observe.Fix(On.May.The1st).If(Sunday).Then.Next(Monday),
+                    Names = NamesBuilder.Make.Add(Langue.FR, "1er mai").AsDictionary(),
+                    Substitute = true
+                },
+                new Rule()
+                {
+                    Expression = ExpressionTree.Date.Catholic.AscensionDay,
+                    Names = NamesBuilder.Make.Add(Langue.FR, "Ascension").AsDictionary()
+                },
+                new Rule()
+                {
+                    Expression = ExpressionTree.Date.Catholic.WhitMonday,
+                    Names = NamesBuilder.Make.Add(Langue.FR, "Lundi de Pentecôte").AsDictionary()
+                },
+                new Rule()
+                {
+                    Expression = ExpressionTree.Date.Catholic.CorpusChristi,
+                    Names = NamesBuilder.Make.Add(Langue.FR, "la Fête-Dieu").AsDictionary()
+                },
+                new Rule()
+                {
+                    Expression = ExpressionTree.Date.Fix(On.August.The15th),
+                    Names = NamesBuilder.Make.Add(Langue.FR, "Assomption").AsDictionary()
+                },
+                new Rule()
+                {
+                    Expression = ExpressionTree.Date.Fix(On.November.The1st),
+                    Names = NamesBuilder.Make.Add(Langue.FR, "Toussaint").AsDictionary()
+                },
+                new Rule()
+                {
+                    Expression = ExpressionTree.Observe.Fix(On.November.The19th).If(Sunday).Then.Next(Monday),
+                    Names = NamesBuilder.Make.Add(Langue.FR, "La Fête du Prince").AsDictionary(),
+                    Substitute = true
+                },
+                new Rule()
+                {
+                    Expression = ExpressionTree.Date.Fix(On.December.The8th),
+                    Names = NamesBuilder.Make.Add(Langue.FR, "Immaculée Conception").AsDictionary()
+                },
+                new Rule()
+                {
+                    Expression = ExpressionTree.Observe.Fix(On.December.The25th).If(Sunday).Then.Next(Monday),
+                    Names = NamesBuilder.Make.Add(Langue.FR, "Noël").AsDictionary(),
+                    Substitute = true
+                }
+            };
+        }
+    }
+}
